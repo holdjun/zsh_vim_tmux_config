15c501a54715a5f6c75dbb06558587c620858040
Hold Johnh <holdjohnh@gmail.com>
2020-05-23 20:51:35 +0800

    [hj] first init

---
+++ README.md
@@ -0,0 +1,12 @@
+# zsh_vim_tmux_config
+
+自动更新 terminal 环境
+
+目前支持
+- 最新的 tmux 使用 oh my tmux
+- 使用 vimrc 安装简单一点的插件
+- 使用 oh my zsh 主题使用自己定制的 和 两个插件
+
+未来可能支持 
+- vscode 的插件同步
+- gdb 插件同步
---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+RED=$(printf '\033[31m')
+GREEN=$(printf '\033[32m')
+YELLOW=$(printf '\033[33m')
+BLUE=$(printf '\033[34m')
+BOLD=$(printf '\033[1m')
+RESET=$(printf '\033[m')
+CARE=$(printf '\033[42m')
+
+# depends
+# install latest tmux
+
+echo "${GREEN}\n\nStart install\n\n${RESET}"
+sudo apt-get install -y git vim zsh fonts-powerline automake bison build-essential pkg-config libevent-dev libncurses5-dev
+
+echo "${GREEN}\n\nStart install latest tmux\n\n${RESET}"
+rm -fr /tmp/tmux
+git clone https://github.com/tmux/tmux.git /tmp/tmux
+cd /tmp/tmux
+git checkout master
+sh autogen.sh
+./configure && make
+sudo make install
+cd -
+rm -fr /tmp/tmux
+echo "${GREEN}\n\ntmux finish!!!!\n\n${RESET}"
+
+# Oh My Tmux!
+echo "${GREEN}\n\nStart install Oh My Tmux!\n\n${RESET}"
+cd
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+cd -
+echo "${GREEN}\n\nOh My tmux finish!!!!\n\n${RESET}"
+
+# install vimrc(if you need)
+echo "${GREEN}\n\nStart install vimrc\n\n${RESET}"
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+echo "${GREEN}\n\nvimrc finish!!!!\n\n${RESET}"
+
+# oh my zsh
+echo "${CARE}\n\nstart install oh my zsh \n PLEASE EXIT ZSH!!!!!\n\n${RESET}"
+echo "${CARE}\n\nremember exit zsh\n\n${RESET}"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s $(which zsh)
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+cp myself.zsh-theme ~/.oh-my-zsh/custom/themes
+echo "ZSH_THEME=\"myself\"" >> ~/.zshrc
+echo "source \$ZSH/oh-my-zsh.sh" >> ~/.zshrc
+echo "source ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+echo "source ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+echo "${CARE}\n\nDon't forget to restart your terminal! finish!!!!!\n\n${RESET}"
